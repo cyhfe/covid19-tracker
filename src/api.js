028ab3bda2765d962cb1884b05572b74bd07e8e1
chenyuhao <cccchenyuhao@gmail.com>
2022-02-25 06:54:34 +0800

    done

--- src/api.js
+++ src/api.js
@@ -2,11 +2,15 @@ import axios from "axios"
 
 const url = "https://covid19.mathdro.id/api"
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changedUrl = url
+  if (country) {
+    changedUrl += `/countries/${country}`
+  }
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url)
+    } = await axios.get(changedUrl)
 
     const modifiedData = {
       confirmed,
@@ -16,5 +20,26 @@ export const fetchData = async () => {
     }
 
     return modifiedData
-  } catch (error) {}
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const fetchCountries = async () => {
+  try {
+    const { data } = await axios.get(`${url}/countries`)
+    console.log(data)
+    return data.countries
+  } catch (error) {
+    console.log(error)
+  }
 }
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,5 +1,81 @@
-function Chart() {
-  return <div>Chart</div>
+import { useEffect, useState } from "react"
+import { fetchDailyData } from "../api"
+import { Line, Bar } from "react-chartjs-2"
+import "../utils/chart"
+import { red, green, purple, blue } from "@mui/material/colors"
+
+function Chart({ country, countryData }) {
+  const [dailyData, setDailyData] = useState(null)
+
+  function renderChart() {
+    return country ? renderBar() : renderLine()
+  }
+
+  function renderBar() {
+    if (!countryData) return null
+    console.log(countryData)
+    const { confirmed, deaths, recovered } = countryData
+    const data = {
+      labels: ["confirmed", "recovered", "deaths"],
+      datasets: [
+        {
+          label: country,
+          data: [confirmed.value, recovered.value, deaths.value],
+          borderColor: blue[500],
+          backgroundColor: blue[200],
+        },
+        // {
+        //   label: "recovered",
+        //   data: recovered.value,
+        //   borderColor: green[500],
+        //   backgroundColor: green[200],
+        // },
+        // {
+        //   label: "deaths",
+        //   data: deaths.value,
+        //   borderColor: red[500],
+        //   backgroundColor: red[200],
+        // },
+      ],
+    }
+    return <Bar data={data} />
+  }
+
+  function renderLine() {
+    if (!dailyData) return null
+    const data = {
+      labels: dailyData.map((d) => d.reportDate),
+      datasets: [
+        {
+          label: "infected",
+          data: dailyData.map((d) => d.confirmed.total),
+          borderColor: purple[500],
+          backgroundColor: purple[200],
+        },
+        {
+          label: "recovered",
+          data: dailyData.map((d) => d.recovered.total),
+          borderColor: green[500],
+          backgroundColor: green[200],
+        },
+        {
+          label: "deaths",
+          data: dailyData.map((d) => d.deaths.total),
+          borderColor: red[500],
+          backgroundColor: red[200],
+        },
+      ],
+    }
+    return <Line data={data} />
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      setDailyData(await fetchDailyData())
+    }
+    getData()
+  }, [])
+  return <div>{renderChart()}</div>
 }
 
 export default Chart
